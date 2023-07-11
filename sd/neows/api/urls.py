fba7fb118ebb6699b7a6d2e24518a234c62fd11f
Aldo <hola564@hotmail.es>
2023-07-10 22:28:12 -0400

    Creación vista plana y conexión por ruta

--- sd/neows/api/urls.py
+++ sd/neows/api/urls.py
@@ -4,6 +4,8 @@ from api import views
 
 urlpatterns = [
     path('test/', views.NeoWSViewTest.as_view(), name='test'),
+    path('indice/', views.index, name='index'),
+    path('distancia/', views.distancia, name='distancia'),
     path('neows/', views.NeoWSView.as_view(), name='neows'),
     #path('neows/<str:init_date>/<str:end_date>/<str:key>', views.NeoWSView.as_view(), name='neows'),
 ]
--- sd/neows/api/views.py
+++ sd/neows/api/views.py
@@ -1,10 +1,11 @@
-#from django.shortcuts import render
+from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 import requests
 import json
 import pymongo
+
 # Create your views here.
 
 # Datos de conexión
@@ -22,9 +23,21 @@ client = pymongo.MongoClient(f"mongodb://{user}:{password}@{host}:{port}/{main_d
 db = client.get_database(database)
 
 
+def index(request):
+    context = {}
+    return render(request, 'index.html', context=context)
+
+
+def distancia(request):
+    #data = objetosHoy()
+    context = {}
+    return render(request, 'distancia.html', context=context)
+
+
 class NeoWSViewTest(APIView):
     def get(self, request):
         return Response({'message': 'hello world'}, status=status.HTTP_200_OK)
+
 
 class NeoWSView(APIView):
     def post(self, request):
@@ -34,18 +47,16 @@ class NeoWSView(APIView):
             end_date = data['end_date']
             key = data['key']
         except Exception as key_error:
-            return Response({'message': 'invalid request 1', 'error':str(key_error)}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': 'invalid request 1', 'error': str(key_error)},
+                            status=status.HTTP_400_BAD_REQUEST)
         print(init_date, end_date, key)
         try:
             url = f'https://api.nasa.gov/neo/rest/v1/feed?start_date={init_date}&end_date={end_date}&api_key={key}'
             print(url)
-            request_neows=requests.get(url)
+            request_neows = requests.get(url)
             print(request_neows)
-            #response_neows=request_neows.json #Aquí está fallando
-            #print(response_neows)
-            response_neows=request_neows.content.decode('utf-8')
+            response_neows = request_neows.content.decode('utf-8')
             print(response_neows)
-            #json_response = json.dumps(response_neows)
         except Exception as e:
-            return Response({'message': 'invalid request 2', 'error':str(e)}, status=status.HTTP_400_BAD_REQUEST)
-        return Response({'message': 'success','data':response_neows},status=status.HTTP_201_CREATED)
+            return Response({'message': 'invalid request 2', 'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'message': 'success', 'data': response_neows}, status=status.HTTP_201_CREATED)
